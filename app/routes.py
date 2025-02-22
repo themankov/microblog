caadecb350e8d8479680f6a8c5e80ce2a8ebec2a
Manannikov Kirill <113590303+themankov@users.noreply.github.com>
2025-02-22 18:31:55 +0300

    добавление шаблонов

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
+
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world"
+    user={'username':'Oleg'}
+    posts=[{'author':{'name':'Anna'},'body':'Beautiful day in Portland!'},{'author':{'name':'Alex'},'body':'Bad day in Huvland!'}]
+    return render_template('index.html', title='Home', user=user, posts=posts)
