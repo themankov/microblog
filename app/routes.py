9f3c687074b6f513297323cdf7d3990096be7ebd
Kirill <manannikov.ka@gmail.com>
2025-02-23 18:38:19 +0300

    Создание форм

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
+from app import forms
 from app import app
-@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form=forms.LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
 @app.route('/index')
 def index():
     user={'username':'Oleg'}
